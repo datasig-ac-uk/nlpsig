e159f580fd858d7a873afd7946b85f2279f4fd40
rchan <rchan@turing.ac.uk>
2022-10-26 15:57:32 +0100

    starting to clean dimensionality reduction class - add TSNE, PCA options

--- src/dimensionality_reduction.py
+++ src/dimensionality_reduction.py
@@ -1,34 +1,66 @@
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 import umap
 import numpy as np
 
 class DimensionalityReduction:
-
-    def __init__(self, method='ppapca', components=5):
+    def __init__(self,
+                 method: str = "ppapca",
+                 components: int = 5) -> None:
         self.method = method
         self.components = components
+        self.embedding = None
 
-    def fit_transform(self, embeddings_sentence):
-
-        if (self.method == 'umap'):
-            #summary of umap hyperparameters
-            #n_neighbors: This parameter controls how UMAP balances local versus global structure in the data. It does this by constraining the size of the local neighborhood
-            #min_dist: controls how tightly UMAP is allowed to pack points together. It, quite literally, provides the minimum distance apart that points are allowed to be in the low dimensional representation.
-            reducer = umap.UMAP(n_components = self.components, min_dist=0.99, n_neighbors=50, metric = 'cosine', random_state=12, transform_seed=12)
-            embeddings_reduced = reducer.fit_transform(embeddings_sentence)
-
-        elif (self.method=='ppapca'):
-            embeddings_reduced = self.ppa_pca(embeddings_sentence, components=self.components, d=3, extra_ppa=False)
-        elif (self.method=='ppapcappa'):
-            embeddings_reduced = self.ppa_pca(embeddings_sentence, components=self.components, d=3, extra_ppa=True)
+    def fit_transform(self,
+                      embeddings_sentence: np.array,
+                      random_state: int = 42) -> None:
+        ### to do: allow passing of arguments into UMAP, TSNE, PCA functions
+        implemented_methods = ["pca",
+                               "umap",
+                               "tsne",
+                               "ppapca",
+                               "ppapcappa"]
+        if self.method in implemented_methods:
+            if self.method == "pca":
+                pca = PCA(n_components = self.components)
+                self.embedding = pca.fit_transform(embeddings_sentence)
+            elif self.method == "umap":
+                reducer = umap.UMAP(n_components = self.components,
+                                    min_dist = 0.99,
+                                    n_neighbors = 50,
+                                    metric = 'cosine',
+                                    random_state = random_state,
+                                    transform_seed = random_state)
+                reducer.fit_transform(embeddings_sentence)
+                self.embedding = reducer.embedding_ 
+            elif self.method == "tsne":
+                tsne = TSNE(n_components = self.components,
+                            learning_rate = 'auto',
+                            random_state = random_state)
+                tsne.fit_transform(embeddings_sentence)
+                self.embedding = tsne.embedding_
+            elif self.method == "ppapca":
+                self.embedding = self.ppa_pca(embeddings_sentence,
+                                              components = self.components,
+                                              dim = 3,
+                                              extra_ppa = False)
+            elif self.method == "ppapcappa":
+                self.embedding = self.ppa_pca(embeddings_sentence,
+                                              components = self.components,
+                                              dim = 3,
+                                              extra_ppa = True)
         else:
-            print('ERROR: need to define a valid dimensionality reduction method')
-        
-        return embeddings_reduced
+            raise NotImplementedError(f"{self.method} is not implemented. "
+                                      f"Try one of the following: " + 
+                                      f"{', '.join(implemented_methods)}.")
+        return self.embedding
 
 
-    def ppa_pca(self,embeddings_sentence, components = 5, d = 3, extra_ppa = False):
-
+    def ppa_pca(self,
+                embeddings_sentence: np.array,
+                components: int = 5,
+                dim: int = 3,
+                extra_ppa: bool = False) -> np.array:
         #PPA NO 1
 
         #Subtract mean embedding
@@ -41,12 +73,10 @@ class DimensionalityReduction:
 
         #Remove top-D components
         z = []
-
         for i, x in enumerate(embeddings_sentence):
-            for u in U1[0:d]:
+            for u in U1[0:dim]:
                 x = x - np.dot(u.transpose(),x) * u
             z.append(x)
-        
         z = np.asarray(z).astype(np.float32)
 
         #Main PCA
@@ -71,7 +101,7 @@ class DimensionalityReduction:
             z_new = []
 
             for i, x in enumerate(embeddings_sentence_fit):
-                for u in U2[1:d]:
+                for u in U2[1:dim]:
                     x = x - np.dot(u.transpose(),x) * u
                 z_new.append(x)
             

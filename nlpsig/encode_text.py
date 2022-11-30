7db937cc502e5b071c9ccda8f2c50d934fda7704
rchan <rchan@turing.ac.uk>
2022-11-30 16:36:54 +0000

    more sentence transformer models possible

--- nlpsig/encode_text.py
+++ nlpsig/encode_text.py
@@ -2,6 +2,7 @@ import pickle
 from typing import Optional
 
 import pandas as pd
+from sentence_transformers import SentenceTransformer
 
 
 class TextEncoder:
@@ -37,10 +38,14 @@ class TextEncoder:
         col_name_text : str, optional
             Column name which has the text in, by default "content"
         model_name : str, optional
-            Name of model to obtain sentence embeddings, by default "all-MiniLM-L6-v2"
+            Name of model to obtain sentence embeddings, by default "all-MiniLM-L6-v2".
+            Other options are:
+            - all-mpnet-base-v2
+            - all-distilroberta-v1
+            - all-MiniLM-L12-v2
         model_args : _type_, optional
-            Any keywords to be passed in to the model, by default have arguments to
-            implement "all-MiniLM-L6-v2" model:
+            Any keywords to be passed in to the model, by default the
+            following arguments to pass into SentenceTransformer():
             {"batch_size": 64,
              "show_progress_bar": True,
              "output_value": "sentence_embedding",
@@ -62,7 +67,12 @@ class TextEncoder:
             self.model_name = model_name
             self.model_args = model_args
             self.model = None
-        self.model_dict = {"all-MiniLM-L6-v2": "sentence_embedding"}
+        self.model_dict = {
+            "all-mpnet-base-v2": "sentence_embedding",
+            "all-distilroberta-v1": "sentence_embedding",
+            "all-MiniLM-L12-v2": "sentence_embedding",
+            "all-MiniLM-L6-v2": "sentence_embedding",
+        }
 
     def encode_sentence_transformer(self) -> None:
         """
@@ -73,7 +83,32 @@ class TextEncoder:
         print(f"[INFO] number of sentences to encode: {len(sentences)}")
         self.embeddings_sentence = self.model.encode(sentences, **self.model_args)
 
-    def load_model(self, force_reload=False) -> None:
+    def set_max_seq_length(self, max_seq_length: int):
+        """
+        Method to change the maximum sequence length in the Sentence Transformer model
+
+        Parameters
+        ----------
+        max_seq_length : int
+            Maximum sequence length to be set (must be a positive integer)
+
+        Raises
+        ------
+        ValueError
+            if max_seq_length is negative
+        """
+        if max_seq_length < 0:
+            raise ValueError("max_seq_length must be a positive integer")
+        self.load_model()
+        if isinstance(self.model, SentenceTransformer):
+            self.model.max_seq_length = max_seq_length
+        else:
+            print(
+                "[INFO] the model is not an instance of the SentenceTransformer class. "
+                + "max_seq_lenght has not been set."
+            )
+
+    def load_model(self, force_reload: bool = False) -> None:
         """
         Loads model into `.model`
 
@@ -97,6 +132,8 @@ class TextEncoder:
                 + "First reset the 'model_name' and 'model_args' attributes "
                 + "if you want to re-load different embeddings"
             )
+            return
+
         detected_model_library = self.detect_model_library()
         if detected_model_library is None:
             raise NotImplementedError(
@@ -104,8 +141,6 @@ class TextEncoder:
                 f"Try one of the following: " + f"{', '.join(self.model_dict.keys())}."
             )
         elif detected_model_library == "sentence_embedding":
-            from sentence_transformers import SentenceTransformer
-
             self.model = SentenceTransformer(self.model_name)
 
     def detect_model_library(self) -> Optional[str]:

ce34cedcbf7824f3868ad6f27d098c561f1978a2
rchan <rchan@turing.ac.uk>
2023-03-28 08:57:37 +0100

    add back classification utils and update __init__

--- nlpsig/__init__.py
+++ nlpsig/__init__.py
@@ -1,28 +1,15 @@
 from .classification_utils import Folds, set_seed
 from .data_preparation import PrepareData
 from .dimensionality_reduction import DimReduce
-from .dyadic_path import DotProductAttention, DyadicSignatures
 from .encode_text import SentenceEncoder, TextEncoder
 from .plot_embedding import PlotEmbedding
-from .pytorch_utils import (
-    KFold_pytorch,
-    testing_pytorch,
-    training_pytorch,
-    validation_pytorch,
-)
 
 __all__ = [
     "Folds",
     "set_seed",
     "PrepareData",
     "DimReduce",
-    "DotProductAttention",
-    "DyadicSignatures",
     "SentenceEncoder",
     "TextEncoder",
     "PlotEmbedding",
-    "KFold_pytorch",
-    "testing_pytorch",
-    "training_pytorch",
-    "validation_pytorch",
 ]
